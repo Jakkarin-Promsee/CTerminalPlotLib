e7a4756683450d8ddf4f736597eb8ba9b2374581
Night <146769399+Jakkarin-Promsee@users.noreply.github.com>
2024-11-03 04:59:02 +0700

    testing function working

--- src/main.c
+++ src/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
-#include "CTerminalPlotLib.h"
+#include "../include/CTerminalPlotLib.h"
+#include "../src/CTerminalPlotLib.c"
 
 int main()
 {
+    int max_param = 5, max_name_size = 20, max_param_size = 100;
+    DataSet *dataSet = ctp_initializeDataSet(max_param, max_name_size, max_param_size);
+
+    if (!dataSet)
+    {
+        printf("Error occur initialize DataSet\n");
+        return 1; // Exit if initialization failed
+    }
+
+    printf("%d", dataSet->max_name_size);
 
     return 0;
 }
